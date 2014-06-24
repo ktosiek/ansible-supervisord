e07f289658e0f1d8f5fa7faa246bed7398c23fd6
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2014-06-25 00:03:19 +0200

    Vagrantfile and example playbook

---
+++ _vagrant/ansible.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Simple wrapper to run ansible against Vagrant
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+export ANSIBLE_HOST_KEY_CHECKING=False  # Don't check known_hosts
+ARGS=(
+    -i "$DIR/.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory"
+    --private-key=~/.vagrant.d/insecure_private_key
+    -u vagrant
+    -e ansible_sudo_password=vagrant
+)
+
+ansible "${ARGS[@]}" "${@}"
