2c22be8ba9665e6e79267ff1d32cffe69b8e7bae
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2014-06-27 21:06:38 +0200

    More verbose test runs

--- _tests/run_tests.sh
+++ _tests/run_tests.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
-# This should be runned on a temporary VM with clean OS
+# This should be run on a temporary VM with clean OS
 set -ex
+
+export ANSIBLE_FORCE_COLOR=yes
 
 ANSIBLE_PLAYBOOK=(
     ansible-playbook -c local
     --sudo
+    -vv
+    --diff
     -i 'localhost ansible_connection=local,')
 
 run_test_playbook() {
     "${ANSIBLE_PLAYBOOK[@]}" "${@}"
-    "${ANSIBLE_PLAYBOOK[@]}" "${@}" | grep ' changed=0 '
+    "${ANSIBLE_PLAYBOOK[@]}" "${@}" | tee /tmp/ansible_output
+    if ! grep -q 'changed=0.*failed=0' /tmp/ansible_output; then
+        echo 'Idempotency test failed'
+        return 1
+    fi
 }
 
 run_test_playbook all.yml
